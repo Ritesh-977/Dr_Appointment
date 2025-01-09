ed611457233839a28fd34cc62b191654cc12a046
Ritesh Singh <yesfirst000@gmail.com>
2025-01-09 23:35:02 +0530

    Little_changes

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -9,14 +9,14 @@ const Login = () => {
     const [state, setState] = useState('Admin')
     const [email,setEmail] = useState('')
     const [password,setPassword] = useState('')
-
     const {setAtoken, backendUrl} = useContext(AdminContext)
     const {setDToken} = useContext(DoctorContext)
 
     const onSubmitHandler = async (event) => {
-      event.preventDefault();
+       event.preventDefault();
       try {
         if(state === 'Admin'){
+
         const {data} = await axios.post(backendUrl + '/api/admin/login', {email, password})
         
         if(data.success){
@@ -29,10 +29,10 @@ const Login = () => {
       } else {
 
         const {data} = await axios.post(backendUrl + '/api/doctor/login',{email,password})
+        
         if(data.success){
           localStorage.setItem('dToken', data.token)
           setDToken(data.token)
-          console.log(data.token)
         } else {
           toast.error(data.message)
         }
