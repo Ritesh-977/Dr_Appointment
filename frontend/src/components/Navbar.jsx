87ee248c39b4086583aeca0eb0ef4f6a8f8c32c9
practice_repo <dropmailtonishan@gmail.com>
2025-09-11 00:29:31 +0530

    fixed frontend

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   }
 
   return (
-    <div className='flex items-cente justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
+    <div className='flex items-cente justify-between text-sm py-4 mb-5 border-b border-b-gray-400 bg-[#bbc2d2]' >
      <img onClick={()=>navigate('/')} className='w-44 cursor-pointer' src={assets.logo} alt="" />
      <ul className='hidden md:flex items-start gap-5 font-medium'>
       <NavLink to='/'>
