39d95774860d6f412db2cdd868159c65b3fd2bf8
Ritesh Singh <yesfirst000@gmail.com>
2025-09-11 03:09:33 +0530

    .

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext()
 const AppContextProvider = (props) => {
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
-    const currencySymbol = '$'
+    const currencySymbol = '₹'
     const [doctors, setDoctors] = useState([])
     const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false)
     const [userData, setUserData] = useState(false)
